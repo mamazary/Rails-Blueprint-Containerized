b4168a5e3d1f67a9fd25fa7acc83c6247d6bfd80
Ari Pratama <arypratama5@gmail.com>
2019-04-24 18:28:07 +0700

    Fizzbuzz

---
+++ misc/fizzbuzz.php
@@ -0,0 +1,14 @@
+<?php
+for ( $i=1; $i<=100; $i++ ){
+  if ($i % 15 == 0){
+    echo "fizzbuzz";
+  } else if ( $i % 3 == 0 ){
+    echo "fizz";
+  } else if ( $i % 5 == 0 ){
+    echo "buzz";
+  } else {
+    echo $i;
+  }
+  echo "\n";
+}
+?>
